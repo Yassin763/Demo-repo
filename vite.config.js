e8d435edb3b1dbad2e2f9331d067e041b25f2b2e
Yassin763 <yoyo_yasin@yahoo.com>
2025-09-22 17:30:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Demo-repo/', // استبدل yassin باسم الريبو بتاعك بالظبط
+  base: '/Demo-repo', // استبدل yassin باسم الريبو بتاعك بالظبط
 })
